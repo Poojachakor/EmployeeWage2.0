295250deca10e0a36fea5b6c4504163617fdad3f
Poojachakor <chakorpooja07@gmail.com>
2023-02-24 15:37:04 +0530

    Add: UC4 file

--- EmpProgram2.0/Program.cs
+++ EmpProgram2.0/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            PartTimeWages.PartTimeWageCheck();
+            SwitchCase.EmployeeWageSwitchCase();
             
 
         }
