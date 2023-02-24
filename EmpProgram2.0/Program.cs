984f55fa3738b2864c567afa9306fe87d26c38b2
Poojachakor <chakorpooja07@gmail.com>
2023-02-24 15:42:54 +0530

    Add: UC5 file

--- EmpProgram2.0/Program.cs
+++ EmpProgram2.0/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            SwitchCase.EmployeeWageSwitchCase();
+           WageForMonths.EmpWageForMonthsCheck();
             
 
         }
