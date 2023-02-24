d567dab799b7291df94b0ae915f8a7aadac65347
Poojachakor <chakorpooja07@gmail.com>
2023-02-24 15:48:24 +0530

    Add: UC6 file

--- EmpProgram2.0/Program.cs
+++ EmpProgram2.0/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-           WageForMonths.EmpWageForMonthsCheck();
+           EmpWageMaxHrs.CalculateMaxWorkingHrs();
             
 
         }
